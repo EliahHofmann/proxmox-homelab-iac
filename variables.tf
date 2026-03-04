085d7ba79b8c22c77824582f6f5ef8eaa9d0ef9a
Eliah Hofmann <github@eliahhofmann.de>
2026-03-04 18:34:37 +0100

    erstellung von main die sich mit telmate zur proxmox verbindet und das erstellen der variable die die main braucht

--- variables.tf
+++ variables.tf
@@ -1,1 +1,15 @@
-. 
+variable "proxmox_api_url" {
+    description = "URL zur Proxmox API"
+    type = string
+}
+
+variable "proxmox_api_token_id" {
+  description = "ID des Proxmox API Tokens"
+  type        = string
+}
+
+variable "proxmox_api_token_secret" {
+  description = "Passwort des Tokens mit sensitive true damit dieses nicht in den Logs erscheint"
+  type        = string
+  sensitive   = true
+}
